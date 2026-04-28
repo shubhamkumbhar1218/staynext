ab656e2417ec7d1d75825d8f1725ba11542fbc3c
Shubham Kumbhar <shubhamkumbhar5671@gmail.com>
2026-04-28 09:55:53 +0530

    Change server port from 3000 to 10000

--- app.js
+++ app.js
@@ -81,7 +81,7 @@ app.use((err, req, res ,next) => {
     res.status(statusCode).render("error.ejs", { err });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 app.listen(PORT, "0.0.0.0", () => {
     console.log("Server running on port", PORT);
